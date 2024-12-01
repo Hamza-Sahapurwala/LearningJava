46bbee4ae026d1b5feb5c34a9b1d10a2a23db3b3
Hamza-Sahapurwala <hamjuzyt@gmail.com>
2024-12-01 06:48:46 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Learning Java
+
+Hello everyone, I am starting my journey of learning Java!!!
+
+Hope this repo helps other beginners too!!!
+
+Thanks for viewing!!!
---
+++ helloworld.java
@@ -0,0 +1,5 @@
+class helloworld{
+    public static void main(String args[]){
+        System.out.println("Hello World!"); // * This prints the test inside (ignore x: as kept by the compiler)
+    }
+}
