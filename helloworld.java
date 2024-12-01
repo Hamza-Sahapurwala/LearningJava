7dcdb2a0505d6b54c763716c630f7ef684d6b96a
Hamza-Sahapurwala <hamjuzyt@gmail.com>
2024-12-01 07:13:54 +0530

    Output Statements

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,13 @@
-class helloworld{
+class helloworld{ // ! class name should always match the name of the file
     public static void main(String args[]){
+
         System.out.println("Hello World!"); // * This prints the test inside (ignore x: as kept by the compiler)
+        
+        // * The ln part in println is used to go to the newline, else the next output line will just print in the same line
+
+        System.out.print("This is print only line.");
+
+        System.out.println("Yes it does!");
+
     }
 }
